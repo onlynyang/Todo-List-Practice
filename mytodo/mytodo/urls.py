b5419123bbb24430773193225447559902583e0c
onlynyang <chica1875@gmail.com>
2024-07-17 10:32:30 +0900

    Feat: Todo-List 초기 세팅 완료 가상환경 및 django 초기세팅 프로젝트 이름: mytodo, 앱 이름: todo todo에서 사용할 파일 생성 기본 todo모델 생성 관리자 페이지 접근 세팅

---
+++ mytodo/mytodo/urls.py
@@ -0,0 +1,22 @@
+"""
+URL configuration for mytodo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin # Django의 어드민 사이트를 사용하기 위해 필요한 모듈을 임포트
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
---
+++ mytodo/todo/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    # 코드에는 없지만 Django가 기본으로 제공하는 pk인 id 필드도 포함되어있음
+    # 이후 id값을 pk로 활용해서 Todo 데이터를 구분할 것
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True) # TextField: 긴 텍스트 저장, 최대 길이 제한 없음
+    created = models.DateTimeField(auto_now_add=True) # 날짜와 시간 데이터 저장
+    complete = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+
+    def __str__(self): # 객체가 문자열로 표현될 때 반환할 값을 정의
+        return self.title # title 필드를 반환한다
+    
+# CharField
+# 타입: 문자열 (짧은 텍스트)
+# 속성:
+# max_length: 필수. 문자열의 최대 길이를 지정합니다.
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+# default: 필드의 기본값을 지정합니다.
+
+# TextField
+# 타입: 문자열 (긴 텍스트)
+# 속성:
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+# default: 필드의 기본값을 지정합니다.
+
+# IntegerField
+# 타입: 정수
+# 속성:
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+# default: 필드의 기본값을 지정합니다.
+
+# BooleanField
+# 타입: 불리언 (True/False)
+# 속성:
+# default: 필드의 기본값을 지정합니다. 기본값은 False입니다.
+
+# DateTimeField
+# 타입: 날짜 및 시간
+# 속성:
+# auto_now: 객체가 저장될 때마다 현재 날짜와 시간으로 업데이트됩니다.
+# auto_now_add: 객체가 처음 생성될 때 현재 날짜와 시간으로 설정됩니다.
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+
+# DateField
+# 타입: 날짜
+# 속성:
+# auto_now: 객체가 저장될 때마다 현재 날짜로 업데이트됩니다.
+# auto_now_add: 객체가 처음 생성될 때 현재 날짜로 설정됩니다.
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+
+# FloatField
+# 타입: 부동 소수점 숫자
+# 속성:
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+# default: 필드의 기본값을 지정합니다.
+
+# EmailField
+# 타입: 이메일 주소 (문자열)
+# 속성:
+# max_length: 문자열의 최대 길이를 지정합니다. 기본값은 254자입니다.
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+# default: 필드의 기본값을 지정합니다.
+
+# URLField
+# 타입: URL (문자열)
+# 속성:
+# max_length: 문자열의 최대 길이를 지정합니다. 기본값은 200자입니다.
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+# default: 필드의 기본값을 지정합니다.
+
+# ForeignKey
+# 타입: 외래 키 (다른 모델과의 관계)
+# 속성:
+# to: 참조할 다른 모델을 지정합니다.
+# on_delete: 참조된 객체가 삭제될 때의 동작을 지정합니다. 예: CASCADE-부모 객체가 삭제되면 자식 객체도 자동으로 삭제, SET_NULL, PROTECT 등.
+# related_name: 역참조 시 사용할 이름을 지정합니다.
+# blank: True일 경우, 필드가 빈 값이어도 유효합니다.
+# null: True일 경우, 데이터베이스에서 이 필드는 NULL 값을 가질 수 있습니다.
+# default: 필드의 기본값을 지정합니다.
